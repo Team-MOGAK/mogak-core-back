3365ec140a635f4bc9932b01fc6589d1c23b60e1
Hyunstone <hyun104467@naver.com>
2026-07-23 23:26:05 +0900

    fix: initialize local test database

--- docker/postgres/init-databases.sh
+++ docker/postgres/init-databases.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -eu
 
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
-  --set=ON_ERROR_STOP=1 \
-  --set=test_database="$MOGAK_TEST_DB" \
-  --command "SELECT format('CREATE DATABASE %I', :'test_database') \gexec"
+PGDATABASE="$POSTGRES_DB" createdb --username "$POSTGRES_USER" "$MOGAK_TEST_DB"
